23c9621c58e6621547e696297dfef5f64de4c0d8
naaccrinc <jwhitley@naaccr.org>
2013-08-13 19:25:22 -0500

    Added GPL header to Python files.

--- Vol2CustomDD.py
+++ Vol2CustomDD.py
@@ -1,3 +1,22 @@
+# Author: Joshua Whitley - NAACCR, Inc.
+# Created: 8/13/2013
+# Modified: 8/13/2013
+#
+# This file is part of vol2_dd_export.
+#
+# vol2_dd_export is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# vol2_dd_export is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with vol2_dd_export.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 from Vol2.Vol2DDExport import DDExporter
 from tkinter import *
